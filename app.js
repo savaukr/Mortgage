14116e3350253f81bbeebad787ad687c9f17aa22
savaukr <sava_ukr@ukr.net>
2021-02-21 14:27:03 +0200

    changed app.js const PORT = process.env.PORT || 3000

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path')
 //const config = require('config')
 //const PORT = config.get('port') || 5000
 const appConfig = require('./config/appConfig.js')
-const PORT = appConfig.port || 3000
+const PORT = appConfig.port || 5000
 
 const app = express()
 
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'production') {
 async function start() {
 	try {
 		//app.listen(PORT, ()=>console.log(`App has been started on port ${PORT}...`))	
-		app.listen(80, err => {
+		app.listen(PORT, err => {
     		if(err) throw err;
 			    console.log("%c Server running", "color: green");
 		});
