29b80fc16dadc03e16a2d29271ec63e1f54a6ddf
savaukr <sava_ukr@ukr.net>
2021-02-21 16:33:30 +0200

    change app.js and .env

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const path = require('path')
 //const config = require('config')
 //const PORT = config.get('port') || 5000
-const appConfig = require('./config/appConfig.js')
-const PORT = appConfig.port || 5000
+//const appConfig = require('./config/appConfig.js')
+//const PORT = appConfig.port || 5000
+
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
@@ -23,7 +25,7 @@ async function start() {
 		//app.listen(PORT, ()=>console.log(`App has been started on port ${PORT}...`))	
 		app.listen(PORT, err => {
     		if(err) throw err;
-			    console.log("%c Server running", "color: green");
+			    console.log("%c Server running on port:", PORT);
 		});
 	} catch (e) {
 		console.log("Server Error ", e.message )
